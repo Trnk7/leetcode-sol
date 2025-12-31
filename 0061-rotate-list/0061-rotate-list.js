440f5abfe43370ad6345969d3ba5bbc9df779567
Tarun kachhawa <tarunkachhawa7@gmail.com>
2025-12-31 16:02:41 +0530

    Time: 2919 ms (5.81%), Space: 57.8 MB (21.42%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function(head, k) {
+    if(head==null||k==0)return head;
+
+    let fast = head;
+    let slow = head;
+    for (let i = 0; i <k; i++) {
+        
+        fast = fast.next;
+        if(fast==null){
+            fast=head;
+        }
+    }
+    while (fast.next !== null) {
+        fast = fast.next;
+        slow = slow.next;
+    }
+    
+    if(slow.next==null)return head;
+    let newHead = slow.next;
+    slow.next=null;
+    fast.next=head
+    return newHead;
+};

48570128dbed95684c078b89f7ee667348c57a81
Tarun kachhawa <tarunkachhawa7@gmail.com>
2026-01-09 16:09:58 +0530

    Time: 5 ms (78.29%), Space: 65.5 MB (90.44%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.js
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.js
@@ -3,14 +3,14 @@
  * @return {number[]}
  */
 var findDuplicates = function(nums) {
-    let freq = new Uint8Array(nums.length+1).fill(0);
-    for(let n of nums){
-        freq[n]++;
-    }
+    
     let ans=[];
-    for(let i=1;i<freq.length;i++){
-        if(freq[i]>1){
-            ans.push(i);
+    for(let i=0;i<nums.length;i++){
+        let ind = Math.abs(nums[i])-1;
+        if(nums[ind]<0){
+            ans.push(Math.abs(nums[i]));
+        }else{
+            nums[ind]=-nums[ind];
         }
     }
     return ans;

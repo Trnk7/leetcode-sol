f3927942cbfd15ad08e6104832d4297fd0b9db52
Tarun kachhawa <tarunkachhawa7@gmail.com>
2026-06-27 11:36:21 +0530

    Time: 859 ms (13.5%), Space: 11.6 MB (30.09%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -4,13 +4,12 @@ public:
         if(i>=word.size())return true;
         if(x<0||x>=board.size()||y<0||y>=board[0].size())return false;
         if(board[x][y]!=word[i])return false;
-        char temp = board[x][y];
         board[x][y]='$';
         bool res = recu(board,word,i+1,x-1,y) ||
                recu(board,word,i+1,x,y-1) ||
                recu(board,word,i+1,x+1,y) ||
                recu(board,word,i+1,x,y+1);
-        board[x][y]=temp;
+        board[x][y]=word[i];
         return res;
     }
     bool exist(vector<vector<char>>& board, string word) {
